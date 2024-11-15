f0d5b52aea0db32858be2bec6a25e248a5ab9aec
Markus Nilsson <markus.nilsson@med.lu.se>
2024-11-15 14:29:10 +0100

    quality of life tweaks

--- dp_node_dcm2nii_on_zips.m
+++ dp_node_dcm2nii_on_zips.m
@@ -6,6 +6,9 @@ classdef dp_node_dcm2nii_on_zips < dp_node_files_to_items
 
             if (nargin < 1), filter_list = {}; end
             if (nargin < 2), filter_mode = 'exclude'; end
+
+            % should be rewritten as a workflow that uses
+            % dp_node_items_from_files instead
             
             obj = obj@dp_node_files_to_items(...
                 dp_node_unzip_dcm2nii(), ...
