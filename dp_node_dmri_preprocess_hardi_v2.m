7e2e9c03364427d7f49da1c7dff4c1650640d0b0
Markus Nilsson <markus.nilsson@med.lu.se>
2025-01-08 16:05:53 +0100

    version2 of standard hardi preproc

---
+++ dp_node_dmri_preprocess_hardi_v2.m
@@ -0,0 +1,27 @@
+classdef dp_node_dmri_preprocess_hardi_v2 < dp_node_workflow
+
+    % holder of biofinder2 standard workflow, version 2
+
+
+    methods
+        
+
+        function obj = dp_node_dmri_preprocess_hardi_v2()
+
+            % expecting ap to be main acquisition
+
+            a = dp_node_dmri_topup2(0);
+
+            b = dp_node_dmri_preprocess_set_defaults('ap_dmri_fn'); b.do_i2o_pass = 1;
+
+            c = dp_node_dmri_bet();     c.do_i2o_pass = 1;       
+            d = dp_node_dmri_denoise(); d.do_i2o_pass = 1;
+            e = dp_node_fsl_eddy(1);
+
+            obj = obj@dp_node_workflow({a,b,c,d,e});            
+
+        end
+
+    end
+
+end
