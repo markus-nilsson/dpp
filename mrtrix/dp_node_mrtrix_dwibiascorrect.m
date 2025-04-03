c2a9b1105a8fe30e4c2f36e8ef3feb8b0ae68e82
Markus Nilsson <markus.nilsson@med.lu.se>
2025-04-03 12:18:18 +0200

    Cleaning

--- mrtrix/dp_node_mrtrix_dwibiascorrect.m
+++ mrtrix/dp_node_mrtrix_dwibiascorrect.m
@@ -4,6 +4,7 @@ classdef dp_node_mrtrix_dwibiascorrect < dp_node_mrtrix & dp_node_dmri
 
         function output = i2o(~, input)
             output.dmri_fn = dp.new_fn(input.op, input.dmri_fn, '_n4');
+            output.bias_fn = dp.new_fn(input.op, input.dmri_fn, '_n4bias');
         end
 
         function output = execute(obj, input, output)
@@ -11,14 +12,14 @@ classdef dp_node_mrtrix_dwibiascorrect < dp_node_mrtrix & dp_node_dmri
             % execute it
             msf_delete(output.dmri_fn);
             msf_mkdir(fileparts(output.dmri_fn));
-            cmd = sprintf('dwibiascorrect ants %s %s -fslgrad %s %s', ...
+            cmd = sprintf('dwibiascorrect ants %s %s -fslgrad %s %s -bias %s', ...
                 input.dmri_fn, ...
                 output.dmri_fn, ...
                 input.bvec_fn, ...
-                input.bval_fn);
+                input.bval_fn, ...
+                output.bias_fn);
 
             obj.syscmd(cmd);
-
 
         end
     end
